fcf2ded41df5c175e3e2e08b994fcc274d43fa94
Pedro Henrique <phenrique.coder@gmail.com>
2025-04-16 18:24:55 -0300

    feat: enhance Education component with dialog for detailed descriptions

--- app/components/section.education.tsx
+++ app/components/section.education.tsx
@@ -7,23 +7,32 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { FaGraduationCap } from 'react-icons/fa6';
 
-export default function Education({
-  props,
-}: {
-  props: any;
-}) {
+export default function Education({ props }: { props: any }) {
   return (
     <section className="mb-8">
-      
-
       {props?.formacoes?.length > 0 && (
-        <section className="pt-6">
-          <h2 className="text-2xl font-semibold mb-4">Formações</h2>
-          <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="pt-6">
+          <div className="flex items-center mb-4">
+            <FaGraduationCap className="w-8 h-8 mr-2" />
+            <h2 className="text-2xl font-semibold">Formação</h2>
+          </div>
+          <div
+            className={`grid gap-4 ${
+              props.formacoes.length === 1 ? 'grid-cols-1' : 'sm:grid-cols-1 md:grid-cols-2'
+            }`}
+          >
             {props.formacoes.map((formacao: any, index: number) => (
               <Card key={index} className="min-h-full flex flex-col justify-between">
-
                 <div>
                   <CardHeader>
                     <CardTitle className="text-lg">{formacao?.instituicao}</CardTitle>
@@ -32,18 +41,34 @@ export default function Education({
                     </CardDescription>
                   </CardHeader>
                   <CardContent>
-                    <p className="text-muted-foreground line-clamp-4">{formacao?.descricao}</p>
+                    <p className="text-muted-foreground line-clamp-3">{formacao?.descricao}</p>
                   </CardContent>
                 </div>
-                <CardFooter>
-                  {/* <Button variant="outline" size="sm">
-                    Mostrar mais
-                  </Button> */}
-                </CardFooter>
+                {formacao?.descricao && (
+                  <CardFooter>
+                    <Dialog>
+                      <DialogTrigger className="text-sm underline text-primary/50 hover:text-primary">
+                        Mostrar mais
+                      </DialogTrigger>
+                      <DialogContent className="w-full lg:max-w-4xl h-auto -mt-20">
+                        <DialogHeader>
+                          <DialogTitle className="sm:text-lg pt-4">
+                            {formacao?.curso} — {formacao?.ano}
+                          </DialogTitle>
+                          <DialogDescription>
+                            <div className="mt-2 sm:text-lg text-muted-foreground text-pretty">
+                              {formacao?.descricao}
+                            </div>
+                          </DialogDescription>
+                        </DialogHeader>
+                      </DialogContent>
+                    </Dialog>
+                  </CardFooter>
+                )}
               </Card>
             ))}
           </div>
-        </section>
+        </div>
       )}
     </section>
   );
