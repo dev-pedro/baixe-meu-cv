a1223172527f427d5d237bb1413c3875069a9be0
Pedro Henrique <phenrique.coder@gmail.com>
2025-04-16 18:24:59 -0300

    feat: enhance Experience component layout with icon and improved structure

--- app/components/section.experience.tsx
+++ app/components/section.experience.tsx
@@ -5,21 +5,27 @@ import {
   AccordionTrigger,
 } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
+import { MdFactory } from 'react-icons/md';
 
 export default function Experience({ props }: { props: any }) {
   return (
     <section className="mb-8">
       {props?.experiences?.length > 0 && (
         <section className="pt-6">
-          <h2 className="text-2xl font-semibold mb-4">Experiências</h2>
+          <div className="flex items-center mb-4">
+            <MdFactory className="w-8 h-8 mr-2" />
+            <h2 className="text-2xl font-semibold">Experiências</h2>
+          </div>
           <Accordion type="single" collapsible className="w-full space-y-2">
             {props?.experiences
               .sort((a: any, b: any) => parseInt(b.inicio) - parseInt(a.inicio)) // Ordena por fim desc
               .map((empresaObj: any, empresaIndex: number) => (
                 <AccordionItem key={empresaIndex} value={`empresa-${empresaIndex}`}>
-                  <AccordionTrigger className='hover:no-underline hover:bg-primary/10 p-2 rounded-xl cursor-pointer'>
+                  <AccordionTrigger className="hover:no-underline hover:bg-primary/10 p-2 rounded-xl cursor-pointer">
                     <div className="text-left flex flex-col md:flex-row items-start md:items-center gap-0 md:gap-2">
-                      <span className="text-lg md:text-xl font-medium no-underline">{empresaObj.empresa}</span>
+                      <span className="text-lg md:text-xl font-medium no-underline">
+                        {empresaObj.empresa}
+                      </span>
                       <span className="hidden md:block text-lg md:text-xl">|</span>
                       <span className="font-bold">
                         {empresaObj.inicio} a {empresaObj.fim}
@@ -27,7 +33,7 @@ export default function Experience({ props }: { props: any }) {
                     </div>
                   </AccordionTrigger>
                   <AccordionContent>
-                    <ul className="flex flex-col gap-4">
+                    <ul className="flex flex-col gap-4 pt-3">
                       {empresaObj.jobs
                         .sort((a: any, b: any) => parseInt(a.inicio) - parseInt(b.inicio)) // Ordena os jobs por fim asc
                         .map((job: any, jobIndex: number) => (
