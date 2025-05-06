7485757fdc6109f252f8dc2f5684b6a3afd6f391
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-06 19:17:35 -0300

    feat: update footer details with new content and formatting

--- app/data/footer.ts
+++ app/data/footer.ts
@@ -1,37 +1,37 @@
-import { IMenuItem, ISocials } from "@/types";
+import { IMenuItem, ISocials } from '@/app/types/types';
 
 export const footerDetails: {
-    subheading: string;
-    quickLinks: IMenuItem[];
-    email: string;
-    telephone: string;
-    socials: ISocials;
+  subheading: string;
+  quickLinks: IMenuItem[];
+  email: string;
+  telephone: string;
+  socials: ISocials;
 } = {
-    subheading: "Um currículo simples e elegante.",
-    quickLinks: [
-        {
-            text: "Funcionalidades",
-            url: "#features"
-        },
-        {
-            text: "Preços",
-            url: "#pricing"
-        },
-        {
-            text: "Testemunhos",
-            url: "#testimonials"
-        }
-    ],
-    email: 'taxconferencepro@gmail.com',
-    telephone: '',
-    socials: {
-        // github: 'https://github.com',
-        // x: 'https://twitter.com/x',
-        // twitter: 'https://twitter.com/Twitter',
-        // facebook: 'https://facebook.com',
-        //linkedin: 'https://www.linkedin.com',
-        youtube: 'http://www.youtube.com/@TaxConferencePro',
-        // threads: 'https://www.threads.net',
-        // instagram: 'https://www.instagram.com',
-    }
-}
+  subheading: 'Um currículo elegante online.',
+  quickLinks: [
+    {
+      text: 'Funcionalidades',
+      url: '#features',
+    },
+    {
+      text: 'Preços',
+      url: '#pricing',
+    },
+    {
+      text: 'Testemunhos',
+      url: '#testimonials',
+    },
+  ],
+  email: 'taxconferencepro@gmail.com',
+  telephone: '',
+  socials: {
+    // github: 'https://github.com',
+    // x: 'https://twitter.com/x',
+    // twitter: 'https://twitter.com/Twitter',
+    // facebook: 'https://facebook.com',
+    // linkedin: 'https://www.linkedin.com',
+    // youtube: 'http://www.youtube.com/@baixemeucv',
+    // threads: 'https://www.threads.net',
+    // instagram: 'https://www.instagram.com',
+  },
+};
