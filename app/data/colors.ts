c6d5fec69b6e1adf83091b2d9ba4db2af5a9cf7f
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-06 19:17:25 -0300

    feat: add colors data for UI theming

---
+++ app/data/colors.ts
@@ -0,0 +1,9 @@
+export const colors = [
+    "#ff8ac4", // oklch(0.78 0.1556 350)
+    "#ffa32d", // oklch(0.79 0.16 67)
+    "#fede1c", // oklch(0.9 0.18 98)
+    "#58cf4e", // oklch(0.76 0.2 142)
+    "#45a3fb", // oklch(0.7 0.1567 250)
+    "#955be3", // oklch(0.6 0.2 300)
+    "#fc425a", // oklch(0.66 0.22 19)
+];
