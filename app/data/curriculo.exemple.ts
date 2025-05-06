ead9d234b8ee03b8e7739406736540f0da7ff4cc
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-06 19:17:29 -0300

    feat: add example curriculum data for user profile

---
+++ app/data/curriculo.exemple.ts
@@ -0,0 +1,190 @@
+export const curriculo = {
+  username: 'pedro',
+  name: 'Pedro Henrique',
+  emailEncrypted: 'RxC9WToKUBhNptE7i59SxTw77pW7Bn9yoPpzoOYoltQrZBDoQiSLGjUf4xtHlkJIvEladr+uc+4v',
+  emailHash: '2e3d0a19dec346cd1f06c6a730716e0e0536bfa6d6d1d72f71bb26c2c8dac741',
+  image: 'https://avatars.githubusercontent.com/u/12634255?v=4',
+  profession: 'Analista de Qualidade Pleno',
+  phone: '/ys3vAXCu78vMbOAaNefDfz6B4ePws4URvybrnCjM3S04eyGal1c56lgexf4',
+  showPhoneInPDF: true,
+  city: 'Hortolândia, SP',
+  bio: 'Entusiasta de tecnologia com foco em desenvolvimento web e aprendizado constante.',
+  public: true,
+  pickColor: 4,
+  portifolio: [
+    {
+      name: 'Siscomex Json Validation',
+      url: 'https://siscomex-json-validation.vercel.app/',
+      descricao:
+        'Validação de arquivos JSON do Siscomex, consulta de atributos por NCM e mais, com interface intuitiva e fácil de usar. O projeto foi desenvolvido utilizando React, Next.js e Tailwind CSS.',
+      tecnologias: ['Node.Js','React', 'Next.js', 'Tailwind CSS', 'TypeScript', 'Vercel'],
+    },
+    {
+      name: 'Currículo Online',
+      url: 'https://curriculo-online.vercel.app/',
+      descricao:
+        'Currículo online, desenvolvido com Next.js, TypeScript e Tailwind CSS. Permite que os usuários criem e compartilhem seus currículos de forma simples e rápida.',
+      tecnologias: ['Next.js', 'Tailwind CSS', 'Vercel', 'PostgreSQL', 'TypeScript'],
+    },
+  ],
+  formacoes: [
+    {
+      instituicao: 'Facudade Anhanguera Campinas',
+      curso: 'Desenvoolvimento de Sistemas',
+      ano: '2015',
+      descricao:
+        'Ao longo da graduação em Desenvolvimento de Sistemas, foram adquiridas competências essenciais em lógica de programação, algoritmos e estruturas de dados, com foco na resolução de problemas computacionais. A formação abrangeu também programação orientada a objetos, desenvolvimento web (front-end e back-end), modelagem e manipulação de bancos de dados relacionais e não relacionais. Além dos conhecimentos técnicos, foram exploradas práticas de engenharia de software, testes, versionamento com Git e segurança da informação. A utilização de metodologias ágeis como Scrum e Kanban favoreceu o trabalho em equipe e a organização de projetos. Projetos integradores e estágios supervisionados proporcionaram experiência prática e aproximaram os aprendizados acadêmicos das demandas do mercado.',
+    },
+  ],
+  experiences: [
+    {
+      empresa: 'Honda Automóveis do Brasil',
+      inicio: '2003',
+      fim: '2011',
+      jobs: [
+        {
+          cargo: 'Team Leader',
+          descricao: 'Liderança de equipe de produção',
+          inicio: '2005',
+          fim: '2011',
+        },
+        {
+          cargo: 'Analista de Qualidade',
+          descricao:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam.',
+          inicio: '2019',
+          fim: '2023',
+        },
+      ],
+    },
+    {
+      empresa: 'GM dos Reis Indústria e Comércio Ltda',
+      inicio: '2012',
+      fim: '2017',
+      jobs: [
+        {
+          cargo: 'Ferramenteiro',
+          descricao:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam.',
+          inicio: '2014',
+          fim: '2017',
+        },
+        {
+          cargo: 'Analista de Processos',
+          descricao:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam.',
+          inicio: '2012',
+          fim: '2014',
+        },
+      ],
+    },
+    {
+      empresa: 'Medical H7 - Soluções Médicas',
+      inicio: '2018',
+      fim: '2023',
+      jobs: [
+        {
+          cargo: 'Sócio Fundador',
+          descricao:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam.',
+          inicio: '2018',
+          fim: '2023',
+        },
+        {
+          cargo: 'Técnico Mecânico e Eletrônico',
+          descricao:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam.',
+          inicio: '2018',
+          fim: '2023',
+        },
+      ],
+    },
+    {
+      empresa: 'Grupo Assist',
+      inicio: '2024',
+      fim: 'Atual',
+      jobs: [
+        {
+          cargo: 'Analista de Suporte e Treinamento',
+          descricao:
+            'Responsável pela gestão de chamados, suporte técnico e elaboração de materiais de treinamento, assegurando que os usuários recebam o suporte necessário para utilizar de forma eficiente as soluções desenvolvidas.',
+          inicio: '2024',
+          fim: '2025',
+        },
+        {
+          cargo: 'Analista de Qualidade Pleno (QA)',
+          descricao:
+            'Estruturação e implementação de estratégias de qualidade no desenvolvimento da plataforma Classist. Criação de testes automatizados e validação contínua das entregas para garantir estabilidade e redução de falhas nas novas funcionalidades. Definição de critérios de aceitação em conjunto com o Tech Lead e equipe de desenvolvimento, visando diminuir bugs em produção e aumentar a confiabilidade da plataforma. Promoção de uma cultura de qualidade na equipe, com práticas como testes regressivos, testes de carga e automação de processos. Atuação focada na redução de retrabalho e aceleração do ciclo de desenvolvimento.',
+          inicio: '2025',
+          fim: 'Atual',
+        },
+      ],
+    },
+  ],
+  courses: [
+    {
+      instituicao: 'Senai',
+      curso: 'Técnico em Mecatrônica',
+      ano: '2011',
+      descricao: 'Aprendizado em mecânica e eletrônica.',
+      online: false,
+    },
+    {
+      instituicao: 'Udemy',
+      curso: 'Curso de JavaScript',
+      ano: '2018',
+      descricao: 'Aprendizado em JavaScript e suas aplicações.',
+      online: true,
+    },
+    {
+      instituicao: 'Udemy',
+      curso: 'Curso de JavaScript',
+      ano: '2018',
+      descricao: 'Aprendizado em JavaScript e suas aplicações.',
+      online: true,
+    },
+    {
+      instituicao: 'Udemy',
+      curso: 'Curso de JavaScript',
+      ano: '2018',
+      descricao: 'Aprendizado em JavaScript e suas aplicações.',
+      online: true,
+    },
+    {
+      instituicao: 'Udemy',
+      curso: 'Curso de React',
+      ano: '2019',
+      descricao: 'Aprendizado em React e suas aplicações.',
+      online: true,
+    },
+  ],
+
+  skills: [
+    'React',
+    'Node.js',
+    'MongoDB',
+    'Next.js',
+    'JavaScript',
+    'TypeScript',
+    'NestJs',
+    'PostgreSQL',
+    'Tailwind CSS',
+    'HTML',
+    'CSS',
+    'Git e GitHub',
+    'Firebase',
+  ],
+  softSkills: [
+    'Comunicação',
+    'Trabalho em equipe',
+    'Resolução de problemas',
+    'Adaptabilidade',
+    'Pensamento crítico',
+    'Gestão do tempo',
+    'Empatia',
+    'Criatividade',
+    'Liderança',
+    'Organização',
+    'Proatividade',
+  ],
+};
