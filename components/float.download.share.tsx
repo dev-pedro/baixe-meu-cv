2ff52be2320df1ea307994aaffad026d826453ae
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-06 19:19:27 -0300

    feat: add FloatingActionMenu component for downloading and sharing resumes

---
+++ components/float.download.share.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { FaDownload, FaRegShareSquare, FaShareAlt } from 'react-icons/fa';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import clsx from 'clsx';
+import { getPickerBg } from '@/utils/colors';
+
+export function FloatingActionMenu({ props }: { props: any }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const { curriculo, userSession }: { curriculo: any; userSession: any } = props;
+  const { bg, hover } = getPickerBg(curriculo.pickColor);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div
+      className={clsx(
+        'fixed bottom-5 right-5 sm:bottom-20 sm:right-20 z-50 transition-all duration-300 ease-in-out',
+        isVisible
+          ? 'opacity-100 translate-y-0 pointer-events-auto'
+          : 'opacity-0 translate-y-8 pointer-events-none'
+      )}
+    >
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button className={`rounded-full w-14 h-14 p-0 shadow`} variant="default">
+            <FaRegShareSquare size={24} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent align="end" className="w-full">
+          <div className="flex flex-col gap-2">
+            <Button
+              variant="ghost"
+              className="justify-start gap-2"
+              onClick={() => console.log('Baixar currículo')}
+            >
+              <FaDownload /> {`Baixar Currículo de ${props.curriculo.name}`}
+            </Button>
+            <Button
+              variant="ghost"
+              className="justify-start gap-2"
+              onClick={() => console.log('Compartilhar')}
+            >
+              <FaShareAlt />Compartilhar este Currículo
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
