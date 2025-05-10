d410c021bb46d130b93bf1ca58bf5f9c11be8406
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-10 16:14:41 -0300

    feat: enhance profile editing with improved form handling and user feedback

--- app/edit-profile/edit.profile.client.tsx
+++ app/edit-profile/edit.profile.client.tsx
@@ -1,110 +1,62 @@
 // app/edit-profile/edit.profile.client.tsx
 'use client';
+import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { createOrUpdateUser } from '@/lib/user';
-import { useEffect, useState } from 'react';
-import { Portifolio, UserInput } from '../types/types';
+import { Graduation, Portfolio, UserSession, DataCreateCurriculoForm } from '../types/types';
 import { toast } from 'sonner';
 import { Camera } from 'lucide-react';
+import Image from 'next/image';
 import { useUser } from '../context/user.context';
 import PortfolioEditor from '@/components/form.portifolio.editor';
 import PickColor from '@/components/form.pick.color';
 import GraduationEditor from '@/components/form.graduations.editor';
 import { FaShieldAlt } from 'react-icons/fa';
+import { ProfileSkeleton } from '@/components/loading.component';
 
-export default function EditProfileClient({ props }: { props?: any }) {
-  const [message, setMessage] = useState<string | null>(null);
-  const { setMyCurriculo } = useUser();
-  if (props?.error) {
-    return <div className="text-red-600 text-center mt-10">{props?.message}</div>;
-  }
+export default function EditProfileClient({ userSession }: { userSession?: UserSession }) {
+  //const { profile, message, error } = userProfile || {};
+  const [formData, setFormData] = useState<DataCreateCurriculoForm>();
+  const { setMyCurriculo, myCurriculo, loading } = useUser();
+  const message = myCurriculo?.message;
+  const error = myCurriculo?.error;
+  console.log('Editor perfil: ', loading);
 
-  const { phone, email, curriculo, userSession } = props;
+  useEffect(() => {
+    if (!loading && myCurriculo?.profile) {
+      const profile = myCurriculo.profile;
 
-  const [formData, setFormData] = useState<UserInput>({
-    username: curriculo?.username || '',
-    name: curriculo?.name || '',
-    email: email || '',
-    image: curriculo.image || userSession?.image || '',
-    phone: phone || '',
-    city: curriculo?.city || '',
-    bio: curriculo?.bio || '',
-    pickColor: curriculo?.pickColor || 1,
-    portfolio: curriculo?.portfolio || [],
-    graduation: curriculo?.graduation || [],
-    showPhoneInPDF: curriculo?.showPhoneInPDF ?? false,
-    showEmailInPDF: curriculo?.showEmailInPDF ?? true,
-    public: curriculo?.public ?? false,
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  // handle para checkboxes com ShadCN
-  const handleCheckboxChange = (name: string) => (checked: boolean) => {
-    setFormData((prev) => ({
-      ...prev,
-      [name]: checked,
-    }));
-  };
-
-  // handle para o portfólio
-  const handlePortfolioChange = (projects: Portifolio[]) => {
-    setFormData((prev) => ({
-      ...prev,
-      portfolio: projects,
-    }));
-  };
-
-  // handle para o pickColor
-  const handlePickColorChange = (color: number) => {
-    setFormData((prev) => ({
-      ...prev,
-      pickColor: color,
-    }));
-  };
-
-  // handle para o graduations
-  const handleGraduationChange = (graduations: any[]) => {
-    setFormData((prev) => ({
-      ...prev,
-      graduation: graduations,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    console.log('Dados do formulário:', formData);
-
-    try {
-      const payload: UserInput = formData; // certifique-se que formData siga o tipo
-
-      const result = await createOrUpdateUser(payload);
-      console.log('Resultado do update:', result);
-
-      // Atualiza o estado do usuário no contexto
-      setMyCurriculo(result);
-      // Atualiza a mensagem de sucesso
-      setMessage(result?.message || 'Atualizado com sucesso!');
-    } catch (err: any) {
-      console.error(err.message);
+      setFormData({
+        username: profile.username || '',
+        name: profile.name || '',
+        email: profile.email || '',
+        phone: profile.phone || '',
+        image: profile.image || userSession?.image || '',
+        bio: profile.bio || '',
+        pickColor: profile.pickColor || 1,
+        profession: profile.profession,
+        city: profile.city || '',
+        showPhoneInPDF: profile.showPhoneInPDF ?? false,
+        showEmailInPDF: profile.showEmailInPDF ?? true,
+        public: profile.public ?? false,
+        portfolio: profile.portfolio || [],
+        graduation: profile.graduation || [],
+        experiences: profile.experiences || [],
+        courses: profile.courses || [],
+        skills: profile.skills || [],
+        softSkills: profile.softSkills || [],
+      });
     }
-  };
+  }, [loading, myCurriculo, userSession]);
 
   // limpa a mensagem após 5 segundos
   useEffect(() => {
     if (message) {
-      if (message.includes('sucesso')) {
+      if (message.includes('sucesso!')) {
         toast.success(message, {
           closeButton: true,
           duration: 5000,
@@ -119,28 +71,90 @@ export default function EditProfileClient({ props }: { props?: any }) {
       }
 
       const timeout = setTimeout(() => {
-        setMessage(null);
+        setMyCurriculo(null);
       }, 5000);
 
       return () => clearTimeout(timeout);
     }
-  }, [message]);
+  }, [message, setMyCurriculo]);
+
+  if (error) {
+    return <div className="mt-10 text-center text-red-600">{message}</div>;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // handle para checkboxes com ShadCN
+  const handleCheckboxChange = (name: string) => (checked: boolean) => {
+    setFormData((prev: any) => ({
+      ...prev,
+      [name]: checked,
+    }));
+  };
+
+  // handle para o portfólio
+  const handlePortfolioChange = (projects: Portfolio[]) => {
+    setFormData((prev: any) => ({
+      ...prev,
+      portfolio: projects,
+    }));
+  };
+
+  // handle para o pickColor
+  const handlePickColorChange = (color: number) => {
+    setFormData((prev: any) => ({
+      ...prev,
+      pickColor: color,
+    }));
+  };
+
+  // handle para o graduations
+  const handleGraduationChange = (graduations: Graduation[]) => {
+    setFormData((prev: any) => ({
+      ...prev,
+
+      graduation: graduations,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log('Dados do formulário:', formData);
+
+    try {
+      const payload: DataCreateCurriculoForm = formData || null; // certifique-se que formData siga o tipo
+
+      const result = await createOrUpdateUser(payload);
+      console.log('Resultado do update:', result);
+
+      // Atualiza o estado do usuário no contexto
+      setMyCurriculo(result);
+    } catch (err: any) {
+      console.error(err?.message);
+    }
+  };
+
+  if (loading) {
+    return <ProfileSkeleton />;
+  }
 
   return (
-    <div className="flex flex-col items-center justify-center mx-auto p-4">
-      <div className="flex flex-col items-center justify-center py-2">
-        <h1 className="text-2xl font-bold mb-4">Editar Currículo</h1>
-        <p className="text-gray-500 mb-4 bg-1">
-          Preencha os campos abaixo para atualizar currículo.
-        </p>
-      </div>
-
+    <div className="flex flex-col items-center justify-center p-4 mx-auto overflow-hidden md:overflow-hidden">
       {/* Imagem do usuário com ícone de edição */}
       <div className="relative flex justify-center mb-4 w-42 h-42">
-        <img
-          src={formData.image || '/default-user.svg'}
+        <Image
+          src={formData?.image || '/default-user.svg'}
           alt="User"
-          className="w-42 h-42 rounded-full border-2 border-gray-300 object-cover"
+          width={168}
+          height={168}
+          className="object-cover border-2 border-gray-300 rounded-full"
         />
 
         {/* Ícone de edição (câmera) */}
@@ -155,7 +169,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
               if (file) {
                 const reader = new FileReader();
                 reader.onloadend = () => {
-                  setFormData((prev) => ({
+                  setFormData((prev: any) => ({
                     ...prev,
                     image: reader.result as string,
                   }));
@@ -167,15 +181,24 @@ export default function EditProfileClient({ props }: { props?: any }) {
         </label>
       </div>
 
+      <div className="flex flex-col items-center justify-center w-full px-4 pb-1">
+        <h1 className="text-2xl font-bold">Meu Currículo</h1>
+        <p className="text-sm text-center text-gray-500 bg-1">
+          Preencha os campos abaixo para criar ou atualizar seu currículo.
+        </p>
+      </div>
+
       <PickColor color={formData?.pickColor || 1} setPickColor={handlePickColorChange} />
 
-      <div className="flex items-center justify-center sm:justify-end mt-2 w-full sm:w-8/12 text-nowrap">
-        <FaShieldAlt className='mr-1'/> dados critografados.
+      <div className="flex items-center justify-center w-full mt-2 sm:justify-end sm:w-8/12 text-nowrap">
+        <FaShieldAlt className="mr-1" /> dados protegidos
       </div>
 
       {/* Área de rolagem para o formulário */}
       <div className="w-full sm:w-8/12">
-        <form onSubmit={handleSubmit} className="space-y-4 flex flex-col m-auto w-full pt-10">
+        {' '}
+        {/* Defina a altura desejada para o scroll */}
+        <form onSubmit={handleSubmit} className="flex flex-col w-full pt-10 m-auto space-y-4">
           {/* Campos de entrada */}
           <div>
             <Label htmlFor="username" className="pb-2">
@@ -185,7 +208,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
               type="text"
               id="username"
               name="username"
-              value={formData.username}
+              value={formData?.username || ''}
               onChange={handleChange}
               required
             />
@@ -198,7 +221,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
               type="text"
               id="name"
               name="name"
-              value={formData.name}
+              value={formData?.name || ''}
               onChange={handleChange}
               required
             />
@@ -212,7 +235,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
               type="email"
               id="email"
               name="email"
-              value={formData.email}
+              value={formData?.email || ''}
               onChange={handleChange}
               required
             />
@@ -226,7 +249,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
               type="text"
               id="phone"
               name="phone"
-              value={formData.phone}
+              value={formData?.phone || ''}
               onChange={handleChange}
             />
           </div>
@@ -238,7 +261,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
               type="text"
               id="city"
               name="city"
-              value={formData.city}
+              value={formData?.city || ''}
               onChange={handleChange}
             />
           </div>
@@ -249,7 +272,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
             <Textarea
               id="bio"
               name="bio"
-              value={formData.bio}
+              value={formData?.bio || ''}
               onChange={handleChange}
               placeholder="Descreva um pouco sobre você..."
             />
@@ -257,7 +280,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
           <div className="flex items-center space-x-2">
             <Checkbox
               id="showPhoneInPDF"
-              checked={formData.showPhoneInPDF}
+              checked={formData?.showPhoneInPDF || false}
               onCheckedChange={handleCheckboxChange('showPhoneInPDF')}
             />
             <Label htmlFor="showPhoneInPDF">Mostrar telefone no PDF</Label>
@@ -265,7 +288,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
           <div className="flex items-center space-x-2">
             <Checkbox
               id="showEmailInPDF"
-              checked={formData.showEmailInPDF}
+              checked={formData?.showEmailInPDF || false}
               onCheckedChange={handleCheckboxChange('showEmailInPDF')}
             />
             <Label htmlFor="showEmailInPDF">Mostrar e-mail no PDF</Label>
@@ -273,7 +296,7 @@ export default function EditProfileClient({ props }: { props?: any }) {
           <div className="flex items-center space-x-2">
             <Checkbox
               id="public"
-              checked={formData.public}
+              checked={formData?.public || false}
               onCheckedChange={handleCheckboxChange('public')}
             />
             <Label htmlFor="public">Tornar público</Label>
@@ -289,8 +312,10 @@ export default function EditProfileClient({ props }: { props?: any }) {
             setGraduation={handleGraduationChange}
           />
 
-          <div className="flex justify-end">
-            <Button type="submit">Salvar</Button>
+          <div className="flex justify-end w-full p-4">
+            <Button type="submit" className="w-full sm:w-2/12">
+              Salvar
+            </Button>
           </div>
         </form>
       </div>
