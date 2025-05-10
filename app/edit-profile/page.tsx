667ef7e0b7e56ce695c8f32a7c5a441eba335bf6
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-10 16:14:46 -0300

    refactor: streamline user session handling and simplify props for EditProfileClient

--- app/edit-profile/page.tsx
+++ app/edit-profile/page.tsx
@@ -5,39 +5,26 @@ import { authOptions } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import EditProfileClient from './edit.profile.client';
 import { getUserByEmailHash } from '@/lib/user';
-import { decryptData } from '@/utils/decrypt.data';
 
 export default async function EditProfilePage() {
   const session = await getServerSession(authOptions);
 
   if (!session) redirect('/');
 
+  const userSession = session.user;
+
   try {
     const result = await getUserByEmailHash(session?.user?.email || '');
-    const user = result?.user || null;
+    console.log('userSession: ', userSession);
     const message = result?.message || '';
     const error = result?.error || false;
-    const { phoneEncrypted, emailEncrypted, ...curriculo } = user || {
-      phoneEncrypted: '',
-      emailEncrypted: '',
-      curriculo: {},
-    };
-
-    const email = emailEncrypted ? await decryptData(emailEncrypted) : '';
-    const phone = phoneEncrypted ? await decryptData(phoneEncrypted) : '';
-
-    const props = {
-      curriculo,
-      email,
-      phone,
-      userSession: session?.user || null,
-      message,
-      error,
-    };
+    if (error) {
+      throw new Error(message);
+    }
 
     return (
       <div>
-        <EditProfileClient props={props} />
+        <EditProfileClient userSession={userSession} />
       </div>
     );
   } catch (error) {
@@ -46,9 +33,7 @@ export default async function EditProfilePage() {
     // Envia o erro como prop para a página client-side lidar com isso
     return (
       <div>
-        <EditProfileClient
-          props={{ error: 'Erro ao carregar dados do usuário. Tente novamente mais tarde.' }}
-        />
+        <EditProfileClient />
       </div>
     );
   }
