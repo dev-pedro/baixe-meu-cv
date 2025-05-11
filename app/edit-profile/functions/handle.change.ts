3aa5f03ccd0b1b08bfa7c77d1723ae8986c982d9
Pedro Henrique <phenrique.coder@gmail.com>
2025-05-11 20:39:09 -0300

    feat: implement handleChange function for dynamic form data handling

---
+++ app/edit-profile/functions/handle.change.ts
@@ -0,0 +1,41 @@
+import { Portfolio } from "@/lib/generated/prisma";
+import { updateFormData } from "./update.form.data";
+import { DataCreateCurriculoForm, Graduation } from "@/app/types/types";
+
+type InputChangeEvent = React.ChangeEvent<
+  HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+>;
+type CheckedState = boolean | 'indeterminate';
+export const handleChange = (
+  setFormData: React.Dispatch<React.SetStateAction<DataCreateCurriculoForm | undefined>>,
+  e: InputChangeEvent | CheckedState | number | Array<Portfolio | Graduation>,
+  name?: string
+) => {
+  // Caso seja o ShadCN Checkbox
+  if (typeof e === 'boolean' || e === 'indeterminate') {
+    if (!name) return; // nome é obrigatório nesse caso
+    updateFormData(setFormData, { [name]: e === true });
+    return;
+  }
+
+  // Caso seja um número (como no caso de PickColor)
+  if (typeof e === 'number') {
+    if (!name) return; // nome é obrigatório nesse caso
+    updateFormData(setFormData, { [name]: e });
+    return;
+  }
+
+  if (Array.isArray(e)) {
+    if (!name) return; // nome é obrigatório nesse caso
+    updateFormData(setFormData, { [name]: [...e] });
+    return;
+  }
+
+  // Caso seja um input tradicional
+  console.log(e);
+  const { name: targetName, type, value } = e.target as HTMLInputElement;
+  const checked = type === 'checkbox' ? (e.target as HTMLInputElement).checked : undefined;
+  const finalValue = type === 'checkbox' ? checked : value;
+
+  updateFormData(setFormData, { [targetName]: finalValue });
+};
