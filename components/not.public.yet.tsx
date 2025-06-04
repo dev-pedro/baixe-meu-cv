b6d52a8dd7ea678fae82d682a78af352cba885c3
Pedro Henrique <phenrique.coder@gmail.com>
2025-06-04 13:58:48 -0300

    refactor: update NotPublishedYet component to use SectionProps and improve profile name handling

--- components/not.public.yet.tsx
+++ components/not.public.yet.tsx
@@ -1,10 +1,11 @@
+import { SectionProps } from '@/app/types/types';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaUser } from 'react-icons/fa6';
 
-export default async function NotPublishedYet({ props }: { props: any }) {
-  const { profile, userSession }: { profile: any; userSession: any } = await props;
+export default async function NotPublishedYet({ props }: { props: SectionProps }) {
+  const { profile, userSession } = await props;
   return (
     <section className="mb-40 md:mb-20 mt-4">
       <div className="flex flex-col items-center text-center pt-20">
@@ -20,8 +21,9 @@ export default async function NotPublishedYet({ props }: { props: any }) {
           <FaUser className="w-44 h-44 rounded-full pb-2" />
         )}
 
-        <p className="pb-6 text-pretty text-xl">
-          <strong>{profile.name}</strong>, ainda não tornou público seu currículo, volte depois.
+        <p className="py-6 text-pretty text-xl">
+          <strong>{profile?.name || userSession?.name}</strong>, ainda não tornou público seu
+          currículo, volte depois.
         </p>
       </div>
     </section>
