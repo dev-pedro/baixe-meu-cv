614913f4b84aa37c5aca475cd222cb4fcf007b8d
Pedro Henrique <phenrique.coder@gmail.com>
2025-06-04 11:49:17 -0300

    feat: add example curriculum data with structured profile and portfolio information

--- app/data/curriculo-example/curriculo.example.ts
+++ app/data/curriculo-example/curriculo.example.ts
@@ -1,149 +1,163 @@
-export const curriculo = {
-  username: 'pedro',
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { PortfolioCategory, PortfolioTag } from '@/lib/generated/prisma';
+
+const imagePath = join(__dirname, '../../data/curriculo-example/image-perfil/perfil.png');
+const imageBuffer = readFileSync(imagePath);
+const imageBase64 = imageBuffer.toString('base64');
+
+export const curriculoExample = {
+  username: 'curriculo-example',
   name: 'Pedro Henrique',
-  emailEncrypted: 'RxC9WToKUBhNptE7i59SxTw77pW7Bn9yoPpzoOYoltQrZBDoQiSLGjUf4xtHlkJIvEladr+uc+4v',
-  emailHash: '2e3d0a19dec346cd1f06c6a730716e0e0536bfa6d6d1d72f71bb26c2c8dac741',
-  image: 'https://avatars.githubusercontent.com/u/12634255?v=4',
+  email: 'pedro.coder@gmail.com',
+  image: `data:image/png;base64,${imageBase64}`,
   profession: 'Analista de Qualidade Pleno',
-  phone: '/ys3vAXCu78vMbOAaNefDfz6B4ePws4URvybrnCjM3S04eyGal1c56lgexf4',
+  phone: '19994513631',
   showPhoneInPDF: true,
   city: 'Hortolândia, SP',
   bio: 'Entusiasta de tecnologia com foco em desenvolvimento web e aprendizado constante.',
   public: true,
   pickColor: 5,
-  portifolio: [
+  portfolio: [
     {
       name: 'Siscomex Json Validation',
       url: 'https://siscomex-json-validation.vercel.app/',
-      descricao:
+      description:
         'Validação de arquivos JSON do Siscomex, consulta de atributos por NCM e mais, com interface intuitiva e fácil de usar. O projeto foi desenvolvido utilizando React, Next.js e Tailwind CSS.',
-      tecnologias: ['Node.Js', 'Next.js', 'Tailwind CSS', , 'Vercel', 'TypeScript'],
+      tags: ['OUTRA'],
+      customTags: ['Node.Js', 'Next.js', 'Tailwind CSS', 'Vercel', 'TypeScript'],
+      category: 'OUTRA',
+      customCategory: 'WEB APP',
     },
     {
       name: 'Baixe Meu CV',
       url: 'https://baixemeucv.com.br/',
-      descricao:
+      description:
         'Baixe Meu CV, desenvolvido com Next.js, TypeScript e Tailwind CSS. Permite que os usuários criem e compartilhem seus currículos de forma simples e rápida.',
-      tecnologias: ['Node.Js', 'Next.js', 'Tailwind CSS', 'Vercel', 'TypeScript', 'PostgreSQL'],
+      tags: [PortfolioTag.OUTRA],
+      customTags: ['Node.Js', 'Next.js', 'Tailwind CSS', 'Vercel', 'TypeScript', 'PostgreSQL'],
+      category: PortfolioCategory.OUTRA,
+      customCategory: 'WEB APP',
     },
   ],
-  formacoes: [
+  graduation: [
     {
-      instituicao: 'Facudade Anhanguera Campinas',
-      curso: 'Tecnologia em Análise e Desenvoolvimento de Sistemas',
-      ano: '2015',
-      descricao:
+      institution: 'Facudade Anhanguera Campinas',
+      name: 'Tecnologia em Análise e Desenvoolvimento de Sistemas',
+      year: '2015',
+      description:
         'Ao longo da graduação em Desenvolvimento de Sistemas, foram adquiridas competências essenciais em lógica de programação, algoritmos e estruturas de dados, com foco na resolução de problemas computacionais. A formação abrangeu também programação orientada a objetos, desenvolvimento web (front-end e back-end), modelagem e manipulação de bancos de dados relacionais e não relacionais. Além dos conhecimentos técnicos, foram exploradas práticas de engenharia de software, testes, versionamento com Git e segurança da informação. A utilização de metodologias ágeis como Scrum e Kanban favoreceu o trabalho em equipe e a organização de projetos. Projetos integradores e estágios supervisionados proporcionaram experiência prática e aproximaram os aprendizados acadêmicos das demandas do mercado.',
       online: false,
     },
   ],
   experiences: [
     {
-      empresa: 'Honda Automóveis do Brasil',
-      inicio: '2003',
-      fim: '2011',
+      company: 'Honda Automóveis do Brasil',
+      start: '2003',
+      end: '2011',
       jobs: [
         {
-          cargo: 'Team Leader',
-          descricao:
+          function: 'Team Leader',
+          description:
             'Atuei como Team Leader, liderando equipe na linha de produção, garantindo qualidade, segurança e eficiência. Fui responsável pela organização de tarefas, treinamento de operadores, resolução de problemas operacionais e apoio às práticas de melhoria contínua no processo produtivo, entre outras atividades.',
-          inicio: '2007',
-          fim: '2011',
+          start: '2007',
+          end: '2011',
         },
         {
-          cargo: 'Operador de Máquinas',
-          descricao:
+          function: 'Operador de Máquinas',
+          description:
             'Na função de Operador de Máquinas de Usinagem, realizando setup, ajuste e operação de equipamentos para fabricação de peças, seguindo rigorosamente os padrões de qualidade e segurança. Fui responsável pela interpretação de desenhos técnicos, controle de medidas e manutenção preventiva básica das máquinas.',
-          inicio: '2005',
-          fim: '2007',
+          start: '2005',
+          end: '2007',
         },
         {
-          cargo: 'Montador',
-          descricao:
+          function: 'Montador',
+          description:
             'Cmo Montador na linha de montagem de automóveis, executando a instalação de componentes e conjuntos conforme padrões técnicos e de qualidade. Fui responsável por seguir instruções de trabalho, utilizar ferramentas específicas e garantir a segurança e eficiência no processo produtivo.',
-          inicio: '2003',
-          fim: '2005',
+          start: '2003',
+          end: '2005',
         },
       ],
     },
     {
-      empresa: 'GM dos Reis Indústria e Comércio Ltda',
-      inicio: '2012',
-      fim: '2017',
+      company: 'GM dos Reis Indústria e Comércio Ltda',
+      start: '2012',
+      end: '2017',
       jobs: [
         {
-          cargo: 'Ferramenteiro',
-          descricao:
+          function: 'Ferramenteiro',
+          description:
             'Na função de Ferramenteiro realizando a fabricação, ajuste e manutenção de dispositivos, ferramentas e moldes utilizados na produção. Fui responsável por interpretar desenhos técnicos, operar máquinas de usinagem e garantir a precisão e qualidade dos componentes.',
-          inicio: '2014',
-          fim: '2017',
+          start: '2014',
+          end: '2017',
         },
         {
-          cargo: 'Analista de Processos',
-          descricao:
+          function: 'Analista de Processos',
+          description:
             'Atuei como Analista de Processos, desenvolvendo, padronizando e otimizando processos produtivos. Fui responsável pela elaboração de instruções de trabalho, acompanhamento no processo produtivo, análise de indicadores, implementação de melhorias e suporte técnico às áreas de produção e qualidade.',
-          inicio: '2012',
-          fim: '2014',
+          start: '2012',
+          end: '2014',
         },
       ],
     },
     {
-      empresa: 'Medical H7 - Soluções Médicas',
-      inicio: '2018',
-      fim: '2023',
+      company: 'Medical H7 - Soluções Médicas',
+      start: '2018',
+      end: '2023',
       jobs: [
         {
-          cargo: 'Sócio Fundador',
-          descricao:
-            'Estando Sócio Fundador da empresa Medical H7 - Soluções Médicas, participei da criação, estruturação e gestão do negócio. Fui responsável pelo desenvolvimento de estratégias comerciais, relacionamento com clientes, coordenação de equipes técnicas e garantia da qualidade dos serviços prestados.',
-          inicio: '2018',
-          fim: '2023',
+          function: 'Sócio Fundador',
+          description:
+            'Estando Sócio Fundador da company Medical H7 - Soluções Médicas, participei da criação, estruturação e gestão do negócio. Fui responsável pelo desenvolvimento de estratégias comerciais, relacionamento com clientes, coordenação de equipes técnicas e garantia da qualidade dos serviços prestados.',
+          start: '2018',
+          end: '2023',
         },
         {
-          cargo: 'Técnico Mecânico e Eletrônico',
-          descricao:
+          function: 'Técnico Mecânico e Eletrônico',
+          description:
             'Como Técnico Mecânico e Eletrônico em equipamentos cirúrgicos, realizando diagnóstico, reparo e calibração de dispositivos, entre outras atividades. Fui responsável por interpretar esquemas técnicos, substituir componentes, realizar testes de funcionalidade e assegurar a conformidade com normas de qualidade e segurança.',
-          inicio: '2018',
-          fim: '2023',
+          start: '2018',
+          end: '2023',
         },
       ],
     },
     {
-      empresa: 'Grupo Assist',
-      inicio: '2024',
-      fim: 'Atual',
+      company: 'Grupo Assist',
+      start: '2024',
+      end: 'Atual',
       jobs: [
         {
-          cargo: 'Analista de Suporte e Treinamento',
-          descricao:
+          function: 'Analista de Suporte e Treinamento',
+          description:
             'Responsável pela gestão de chamados, suporte técnico e elaboração de materiais de treinamento, assegurando que os usuários recebam o suporte necessário para utilizar de forma eficiente as soluções desenvolvidas.',
-          inicio: '2024',
-          fim: '2025',
+          start: '2024',
+          end: '2025',
         },
         {
-          cargo: 'Analista de Qualidade Pleno (QA)',
-          descricao:
+          function: 'Analista de Qualidade Pleno (QA)',
+          description:
             'Estruturação e implementação de estratégias de qualidade no desenvolvimento da plataforma Classist. Criação de testes automatizados e validação contínua das entregas para garantir estabilidade e redução de falhas nas novas funcionalidades. Definição de critérios de aceitação em conjunto com o Tech Lead e equipe de desenvolvimento, visando diminuir bugs em produção e aumentar a confiabilidade da plataforma. Promoção de uma cultura de qualidade na equipe, com práticas como testes regressivos, testes de carga e automação de processos. Atuação focada na redução de retrabalho e aceleração do ciclo de desenvolvimento.',
-          inicio: '2025',
-          fim: 'Atual',
+          start: '2025',
+          end: 'Atual',
         },
       ],
     },
   ],
   courses: [
     {
-      instituicao: 'Senai',
-      curso: 'TWI - Ensino correto de um trabalho - Fase I e II',
-      ano: '2011',
-      descricao:
+      institution: 'Senai',
+      name: 'TWI - Ensino correto de um trabalho - Fase I e II',
+      year: '2011',
+      description:
         'Curso TWI – Ensino Correto de um Trabalho (Fase I e II) realizado no SENAI, com foco no desenvolvimento de habilidades para padronizar, instruir e treinar colaboradores de forma eficiente, entre outras atividades. Aprendi técnicas de desdobramento de tarefas, elaboração de instruções claras, acompanhamento de desempenho e aplicação de métodos para garantir a qualidade, segurança e produtividade no ambiente de trabalho.',
       online: false,
     },
     {
-      instituicao: 'Heller Máquinas Operatrizes',
-      curso: 'Programação CNC',
-      ano: '2007',
-      descricao: 'Curso de Programação CNC, com foco na criação e otimização de programas para máquinas de comando numérico computadorizado. Aprendi a interpretar desenhos técnicos, definir estratégias de usinagem, elaborar códigos G e M, ajustar parâmetros de corte e garantir a precisão e eficiência dos processos produtivos.',
+      institution: 'Heller Máquinas Operatrizes',
+      name: 'Programação CNC',
+      year: '2007',
+      description:
+        'Curso de Programação CNC, com foco na criação e otimização de programas para máquinas de comando numérico computadorizado. Aprendi a interpretar desenhos técnicos, definir estratégias de usinagem, elaborar códigos G e M, ajustar parâmetros de corte e garantir a precisão e eficiência dos processos produtivos.',
       online: false,
     },
   ],
@@ -161,10 +175,8 @@ export const curriculo = {
     'Resolução de Problemas',
     'Liderança e Motivação de Equipe',
     'Normas de Qualidade e Segurança',
-    'Lean Manufacturing e Kaizen'
-]
-,
-  
+    'Lean Manufacturing e Kaizen',
+  ],
   softSkills: [
     'Comunicação',
     'Trabalho em Equipe',
@@ -177,6 +189,6 @@ export const curriculo = {
     'Criatividade e Inovação',
     'Liderança',
     'Organização e Planejamento Estratégico',
-    'Proatividade e Iniciativa'
-]
+    'Proatividade e Iniciativa',
+  ],
 };
