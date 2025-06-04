97e58588b27932274798806eb0a615a7956d7ee2
Pedro Henrique <phenrique.coder@gmail.com>
2025-06-04 11:49:39 -0300

    refactor: specify session type as UserSession for improved type safety

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import HomeClient from './home.client';
+import { UserSession } from './types/types';
 
 export default async function HomePage() {
-  const session = await getServerSession(authOptions);
+  const session: UserSession = await getServerSession(authOptions);
 
   return <HomeClient session={session} />;
 }
