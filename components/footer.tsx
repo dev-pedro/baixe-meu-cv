fabc2e70daa76754026600892208277c3c2c13d8
dev-pedro <phenrique.coder@gmail.com>
2025-07-27 11:21:53 -0300

    refactor: improve footer layout and update MenuBarUser display name handling

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,22 +7,16 @@ import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-foreground pt-20 text-center">
-      <div className="max-w-7xl w-full mx-auto">
-        <div className="flex flex-col items-center">
-          <Link href="/" className="flex items-center gap-2 mb-0">
-            <Image
-              src={logo}
-              alt="logo"
-              width={40} // em pixels
-              height={40}
-              className="w-36 h-auto"
-            />
+    <footer className="pt-20 text-center text-foreground">
+      <div className="w-full mx-auto max-w-7xl">
+        <div className="relative flex flex-col items-center ">
+          <Link href="/" className="flex items-center h-20 gap-2 mb-0">
+            <Image src={logo} alt="logo" fill />
           </Link>
-          <p className="text-foreground-accent">{footerDetails.subheading}</p>
         </div>
+        <p className="text-foreground-accent">{footerDetails.subheading}</p>
       </div>
-      <div className="my-2 md:text-center text-foreground-accent px-6">
+      <div className="px-6 my-2 md:text-center text-foreground-accent">
         <p>
           Copyright &copy; {new Date().getFullYear()} {siteDetails.siteName}. All rights reserved.
         </p>
--- components/menu.bar.tsx
+++ components/menu.bar.tsx
@@ -12,7 +12,6 @@ import {
   MenubarSeparator,
   MenubarTrigger,
 } from './ui/menubar';
-import { TbMenu2 } from 'react-icons/tb';
 import { FaUserEdit } from 'react-icons/fa';
 import { MdFindInPage } from 'react-icons/md';
 import Image from 'next/image';
@@ -29,6 +28,10 @@ export function MenuBarUser({ userSession }: { userSession: UserSession }) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [imageLoaded, setImageLoaded] = useState(false);
+  const displayName = (profile?.name || userSession?.name || 'Usuário')
+    .split(' ')
+    .slice(0, 2)
+    .join(' ');
 
   const handleLogin = async () => {
     setLoading(true);
@@ -43,16 +46,15 @@ export function MenuBarUser({ userSession }: { userSession: UserSession }) {
   };
 
   return (
-    <Menubar className="sticky top-0 z-50 justify-between w-full h-full px-4 py-4 mx-auto border-none shadow-none sm:px-14 bg-background sm:justify-end">
+    <Menubar className="sticky top-0 z-50 justify-between w-full h-full px-4 py-4 mx-auto border-none shadow-xs sm:px-14 bg-background sm:justify-end">
       <div className="flex items-center justify-between w-full">
-        <Link href="/">
-          <Image
-            src={logo}
-            alt="logo"
-            width={40}
-            height={40}
-            className={`w-28 h-auto sm:block ${userSession ? 'hidden' : 'block'}`}
-          />
+        <Link
+          href="/"
+          className={`relative w-auto h-full sm:block ${userSession ? 'hidden' : 'block'}`}
+        >
+          <div className="w-32 h-12">
+            <Image src={logo} alt="logo" fill />
+          </div>
         </Link>
 
         {loading ? (
@@ -72,11 +74,11 @@ export function MenuBarUser({ userSession }: { userSession: UserSession }) {
               </Button>
             ) : (
               <div className="flex items-center justify-end w-full gap-2 text-lg">
-                <MenubarTrigger className="flex gap-2 items-center">
-                  Olá, {profile?.name || userSession?.name || 'Usuário'}
+                <MenubarTrigger className="flex items-center gap-2">
+                  Olá, {displayName}
                   {/* Fallback enquanto a imagem carrega */}
                   {!imageLoaded && (
-                    <div className="w-10 h-10 bg-muted rounded-full animate-pulse" />
+                    <div className="w-10 h-10 rounded-full bg-muted animate-pulse" />
                   )}
                   <Image
                     src={getImageSrc()}
@@ -91,11 +93,11 @@ export function MenuBarUser({ userSession }: { userSession: UserSession }) {
                   />
                 </MenubarTrigger>
 
-                <MenubarContent>
+                <MenubarContent alignOffset={90}>
                   <MenubarItem onClick={() => router.push(`/${profile?.username}`)}>
                     <MdFindInPage /> Ver meu currículo
                   </MenubarItem>
-                  <MenubarItem onClick={() => router.push('/edit-profile')}>
+                  <MenubarItem onClick={() => router.push('/meu_perfil')}>
                     <FaUserEdit /> Editar meu currículo
                   </MenubarItem>
                   <MenubarSeparator />
